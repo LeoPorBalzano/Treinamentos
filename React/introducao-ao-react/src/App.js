c079551ee2c4613da69933b591445821e52265b0
LeoPorBalzano <leonardo.balzano@acct.global>
2023-04-11 11:33:55 -0300

    Bland First Hello Word

---
+++ React/introducao-ao-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OlaMundo />
+    </div>
+  );
+}
+
+function OlaMundo() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
